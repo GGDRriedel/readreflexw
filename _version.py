1db4e7d0cf85073f4df5c295acab7a361a23315e
GGDRriedel <riedel@geophysik-ggd.com>
2022-02-22 14:59:35 +0100

    added setup.py in preparation for setuptools, not functional yet

---
+++ _version.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Feb 22 14:51:16 2022
+
+@author: Riedel
+"""
+
+version = "0.0.1"
---
+++ setup.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Feb 22 14:50:22 2022
+
+@author: Riedel
+"""
+# =============================================================================
+# 
+# import setuptools
+# from readreflex import _version
+# 
+# with open("README.md", "r") as fh:
+#     long_description = fh.read()
+# 
+# setuptools.setup(
+#     name="readreflexw",
+#     version=_version.version,
+#     author="Paul-Benjamin Riedel",
+#     author_email="riedel@geophysik-ggd.com",
+#     license='MIT',
+#     description="Python library to read and handle data written with the Software reflexW by Sandmeier Geophysical Research",
+#     long_description=long_description,
+#     long_description_content_type="text/markdown",
+#     url="https://github.com/GGDRriedel/readreflexw",
+#     packages=setuptools.find_packages(),
+#     install_requires=['segyio', 'tqdm', 'numpy', 'obspy', 'matplotlib', 'pandas', 'h5py', 'scipy', 'librosa'],
+#     entry_points='''
+#         [console_scripts]
+#         readreflex=readreflex.readreflex:main
+#     ''',
+#     classifiers=[
+#         "Programming Language :: Python :: 3",
+#         "License ::MIT
+#         "Operating System :: Windows",
+#         "Framework :: Matplotlib",
+#         "Topic :: Scientific/Engineering :: Physics",
+#         "Intended Audience :: Science/Research",
+#         "Natural Language :: English",
+#         
+#     ],
+# )
+# =============================================================================
