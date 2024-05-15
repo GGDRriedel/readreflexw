032c7b8e49bdf0dbb228f2d84fdbfd0101371d1b
GGDRriedel <riedel@geophysik-ggd.com>
2024-05-15 14:53:54 +0200

    Fixed plotting issues with new time representation in absolute values

--- test.py
+++ test.py
@@ -15,4 +15,9 @@ testradargram.read_data_file(r"exampledata/testdata.DAT",version=9)
 
 print(testradargram.header)
 
+#the standardplot 
 testradargram.radarplot()
+
+#note how it plots from the Beginning time set in the file
+
+testradargram.traces.shape
